750b303a5052e573f0d23134dd3168d57736311d
Andrea <a.battaglia79@gmail.com>
2020-06-10 18:11:52 +0200

    bugfix get_serial_number

--- sensors/System.py
+++ sensors/System.py
@@ -1,4 +1,7 @@
+from enviroplus import gas
+
 from flask import Blueprint
+from ReturnValue import return_simple, return_map
 system_blueprint = Blueprint('system', __name__)
 
 
@@ -6,4 +9,6 @@ system_blueprint = Blueprint('system', __name__)
 @system_blueprint.route("/id")
 def get_serial_number():
     #/proc/cpuinfo shows RPi Serial Number on 32bit kernel only!
-    return open('/sys/firmware/devicetree/base/serial-number', 'r').strip()
+    with open('/sys/firmware/devicetree/base/serial-number', 'r') as f:
+        first_line = f.readline()
+        return first_line.strip()
