9ffb3951e4ae1037249adcbcfa16d4d36a1ccc5c
Andrea <a.battaglia79@gmail.com>
2020-06-10 18:18:14 +0200

    bugfix particulates

--- sensors/Particulates.py
+++ sensors/Particulates.py
@@ -9,6 +9,7 @@ from pms5003 import PMS5003, ReadTimeoutError
 
 pms5003 = PMS5003()
 
+
 # PM1.0 ug/m3 (ultrafine particles):
 # PM2.5 ug/m3 (combustion particles, organic compounds, metals):
 # PM10 ug/m3  (dust, pollen, mould spores):
@@ -23,18 +24,23 @@ pms5003 = PMS5003()
 # >10um in 0.1L air:
 @particulates_blueprint.route("/")
 def all():
+    
+            readings = pms5003.read()
+            logging.info(readings)
+            print(readings)
+            
             returnDict = {
-                'PM1_0':pms5003.read()[0],
-                'PM2_5':pms5003.read()[1],
-                'PM10':pms5003.read()[2],
-                'PM1_0_atm':pms5003.read()[3],
-                'PM2_5_atm':pms5003.read()[4],
-                'PM10_atm':pms5003.read()[5],
-                'gt0_3um':pms5003.read()[6],
-                'gt0_5um':pms5003.read()[7],
-                'gt1_0um':pms5003.read()[8],
-                'gt2_5um':pms5003.read()[9],
-                'gt5_0um':pms5003.read()[10],
-                'gt10um':pms5003.read()[11]
+                'PM1_0':readings[0],
+                'PM2_5':readings[1],
+                'PM10':readings[2],
+                'PM1_0_atm':readings[3],
+                'PM2_5_atm':readings[4],
+                'PM10_atm':readings[5],
+                'gt0_3um':readings[6],
+                'gt0_5um':readings[7],
+                'gt1_0um':readings[8],
+                'gt2_5um':readings[9],
+                'gt5_0um':readings[10],
+                'gt10um':readings[11]
             }
             return return_map(returnDict)
